0f01cc7f9010a196659cae4a4262bc9f84fa410f
Austin Hervias <iaustin7@gmail.com>
2019-11-16 14:27:10 -0500

    Added Cloudflare provider to Terraform

--- terraform/website.tf
+++ terraform/website.tf
@@ -66,9 +66,48 @@ resource "digitalocean_floating_ip" "website" {
   region     = digitalocean_droplet.webserver.region
 }
 
-
 # Configure SSH key
 # resource "digitalocean_ssh_key" "default" {
 #   name       = "SSH key"
 #   public_key = file("/path/to/.ssh/id_rsa.pub")
 # }
+
+# Set in the terraform.tfvars file
+variable "cloudflare_email" {}
+variable "cloudflare_api_key" {}
+variable "cloudflare_zone_id" {}
+
+# Configure Cloudflare Provider
+provider "cloudflare" {
+  version = "~> 2.0"
+  email   = "${var.cloudflare_email}"
+  api_key = "${var.cloudflare_api_key}"
+}
+
+# Configure DNS records
+resource "cloudflare_record" "root" {
+  zone_id = "${var.cloudflare_zone_id}"
+  name    = "@"
+  value   = digitalocean_floating_ip.website.ip_address
+  type    = "A"
+  ttl     = 1
+  proxied = "true"
+}
+
+resource "cloudflare_record" "www" {
+  zone_id = "${var.cloudflare_zone_id}"
+  name    = "www"
+  value   = "austinhervias.xyz"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = "true"
+}
+
+resource "cloudflare_record" "ssh" {
+  zone_id = "${var.cloudflare_zone_id}"
+  name    = "ssh"
+  value   = "austinhervias.xyz"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = "false"
+}
