0cc27f1b7de1da12af8a9eaafc67fa1d2ead0a1c
Austin Hervias <iaustin7@gmail.com>
2019-11-16 23:24:48 -0500

    Fixed start Portainer command

--- shell/start_services.sh
+++ shell/start_services.sh
@@ -2,6 +2,6 @@
 
 docker run -d -p 80:80 --name flask-website ahervias77/flask-website
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p --name portainer 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 8000:8000 -p 9000:9000 --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
 # Get Datadog docker agent on the Integrations/Agent page (contains API Key)
